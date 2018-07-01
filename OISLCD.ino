5c4b41aebca413f2c59461e740bb2f76bb363d9b
nudle <leaheatsnudle@gmail.com>
2018-07-01 23:51:40 +0100

    commenting stuff

--- OISLCD.ino
+++ OISLCD.ino
@@ -8,35 +8,30 @@ ObjectsInSpace OIS(Serial, 4);
 
 void setup() {
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  Serial.begin(9600);
-  OIS.begin();
+  lcd.begin(16, 2); //specific for a 16x2 LCD screen (change this for different sizes)
+
+  Serial.begin(9600); //must be 9600
+  OIS.begin(); //begin the handshake/syncronisation with the game
 
   //handshake
-  OIS.registerFloat(CURRENT_SPEED, speedCallback);
+  OIS.registerFloat(CURRENT_SPEED, speedCallback); //register the command to get the speed from the game. check below for the callback
   OIS.registerInt(DIRECTION, headingCallback);
   OIS.registerInt(POWER_LEVEL, powerCallback);
   OIS.registerBool(MAIN_ENGINE_BURNING, burningCallback);
 
-  OIS.activate();
-  
-  // Print a message to the LCD.
-  //lcd.print("Hello, World!");
+  OIS.activate(); //stop syncing and ACTIVATE
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  //lcd.setCursor(0, 0);
-  OIS.update();
+  OIS.update(); //required to keep getting info from the game.
 
 }
 
 void speedCallback(int channel, float data)
 {
-  lcd.setCursor(0,0);
-  lcd.print("SPD:");
-  lcd.print(data);
+  lcd.setCursor(0,0); //set the cursor to the top left of the screen (0,0)
+  lcd.print("SPD:"); //print something to the screen! this automatically moves the cursor
+  lcd.print(data); //so we dont need to move it again before the next printout
 }
 
 void headingCallback(int channel, int data)
